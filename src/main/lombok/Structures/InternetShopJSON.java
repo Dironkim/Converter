351b14161ceefc36414299f636a786a8b4ab5611
dd <dd@LAPTOP>
2023-11-29 11:23:56 +0300

    refactored json base class with lombok

--- src/main/lombok/Structures/InternetShopJSON.java
+++ src/main/lombok/Structures/InternetShopJSON.java
@@ -1,50 +1,32 @@
 package Structures;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+
 import java.util.List;
 
+@Data
 public class InternetShopJSON {
 
     private List<Category> categories;
 
-    public List<Category> getCategories() {
-        return categories;
-    }
-
-    public void setCategories(List<Category> categories) {
-        this.categories = categories;
-    }
-
+    @Data
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
     public static class Category {
 
         private int id;
         private String name;
         private List<Product> products;
-
-        public int getId() {
-            return id;
-        }
-
-        public void setId(int id) {
-            this.id = id;
-        }
-
-        public String getName() {
-            return name;
-        }
-
-        public void setName(String name) {
-            this.name = name;
-        }
-
-        public List<Product> getProducts() {
-            return products;
-        }
-
-        public void setProducts(List<Product> products) {
-            this.products = products;
-        }
     }
 
+    @Data
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
     public static class Product {
 
         private int id;
@@ -52,101 +34,26 @@ public class InternetShopJSON {
         private int price;
         private String description;
         private List<Promotion> promotions;
-
-        public int getId() {
-            return id;
-        }
-
-        public void setId(int id) {
-            this.id = id;
-        }
-
-        public String getName() {
-            return name;
-        }
-
-        public void setName(String name) {
-            this.name = name;
-        }
-
-        public int getPrice() {
-            return price;
-        }
-
-        public void setPrice(int price) {
-            this.price = price;
-        }
-
-        public String getDescription() {
-            return description;
-        }
-
-        public void setDescription(String description) {
-            this.description = description;
-        }
-
-        public List<Promotion> getPromotions() {
-            return promotions;
-        }
-
-        public void setPromotions(List<Promotion> promotions) {
-            this.promotions = promotions;
-        }
     }
 
+    @Data
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
     public static class Promotion {
 
         private int id;
         private String name;
         private Discount discount;
-        private boolean freeShipping; // Флаг бесплатной доставки
-
-        public int getId() {
-            return id;
-        }
-
-        public void setId(int id) {
-            this.id = id;
-        }
-
-        public String getName() {
-            return name;
-        }
-
-        public void setName(String name) {
-            this.name = name;
-        }
-
-        public Discount getDiscount() {
-            return discount;
-        }
-
-        public void setDiscount(Discount discount) {
-            this.discount = discount;
-        }
-
-        public boolean isFreeShipping() {
-            return freeShipping;
-        }
-
-        public void setFreeShipping(boolean freeShipping) {
-            this.freeShipping = freeShipping;
-        }
+        private boolean freeShipping;
     }
 
+    @Data
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
     public static class Discount {
 
         private int percentage;
-
-
-        public int getPercentage() {
-            return percentage;
-        }
-
-        public void setPercentage(int percentage) {
-            this.percentage = percentage;
-        }
-
     }
 }
-
